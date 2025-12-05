0ef5a8a24a6943802db35ada31cd057fd2a1ed0a
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-12-05 12:29:56 +0000

    chores:add the need functions for ingesting, get embedding and saving the embedding to the pinecone database

---
+++ src/lib/ingest-doc.ts
@@ -0,0 +1,32 @@
+import { extractTextFromDocx } from "./doc-reader.js";
+import { splitText } from "./doc-splitter.js";
+import { getEmbedding } from "../config/openRouter.js";
+import { pineconeIndex } from "../config/pinecone.js";
+import { nanoid } from "nanoid";
+
+async function ingestDocx(filepath:string){
+    const rawText = await extractTextFromDocx(filepath)
+    const chunks = splitText(rawText)
+
+    for(let i =0; i < chunks.length; i++){
+        const chunk = chunks[i]
+        const embedding = await getEmbedding(chunk)
+
+        await pineconeIndex.upsert([
+            {
+                id:nanoid(),
+                values:embedding,
+                metadata: {
+                    text:chunk,
+                    source:filepath,
+                    chunk_index:i,
+                }
+            }
+        ])
+    }
+
+    console.log(`Ingested ${chunks.length} chunks from ${filepath}`)
+}
+
+
+ingestDocx("src/configs/data/JULIE.docx")
