971949dacc49c49fed91a799915f6bef7756d392
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-12-05 12:43:35 +0000

    feat: Add AI query route with embedding and Pinecone integration, and configure Hono with middlewares.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { secureHeaders } from 'hono/secure-headers'
 
-const app = new Hono()
+import query from "./routes/query.js"
+
+
+const app = new Hono().basePath("/api/v1/")
+
+//middlewares
+app.use(logger())
+app.use(secureHeaders())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+//query route
+app.route("/", query)
+//webhook route
+// app.post("/webhook", webhook)
 
 serve({
   fetch: app.fetch,
