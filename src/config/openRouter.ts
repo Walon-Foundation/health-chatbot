6dde1159aa049598328bbd927a1e263a93817306
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-12-05 12:43:18 +0000

    feat: Implement AI query endpoint with RAG using Pinecone and OpenRouter streaming, and add Hono middlewares.

--- src/config/openRouter.ts
+++ src/config/openRouter.ts
@@ -15,3 +15,34 @@ export async function getEmbedding(text: string): Promise<number[]> {
   return response.data[0].embedding as number[];
 }
 
+
+export async function generateAnswerStream(question: string, context: string) {
+  const response = await openRouter.chat.send({
+    model: 'openai/gpt-oss-20b:free',
+    stream: true,
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a helpful assistant. Use the context to answer the question, and just send the answer — no extra explanation.',
+      },
+      {
+        role: 'user',
+        content: `Context:\n${context}\n\nQuestion: ${question}`,
+      },
+    ],
+  });
+
+  return response; // async iterable!
+}
+
+
+await openRouter.chat.send({
+  model: 'openai/gpt-4o',
+  messages: [
+    {
+      role: 'user',
+      content: 'What is the meaning of life?',
+    },
+  ],
+  stream: false,
+});
