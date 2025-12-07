c9b5dea616624e87a38c88c6ee4ee6f491ecbd9a
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-12-07 12:14:38 +0000

    feat: add biome configuration and update package dependencies for improved formatting and linting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./src/config/env.js";
 
 export default defineConfig({
-  out: "./db/drizzle/migrations",
-  schema: "./db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+	out: "./db/drizzle/migrations",
+	schema: "./db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
 });
